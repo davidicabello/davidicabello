048d60d3cba3175c297d53caaa1c1347aca43e95
davidlp90 <davidivan.eda@gmail.com>
2021-10-20 19:41:58 -0300

    update nuevo
    update nuevo

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ window.onscroll = function () {
     if (prevScrollpos > currentScrollPos) {
         document.getElementById("navbar").style.top = "0";
     } else {
-        document.getElementById("navbar").style.top = "-60px";
+        document.getElementById("navbar").style.top = "-100px";
     }
     prevScrollpos = currentScrollPos;
 }
